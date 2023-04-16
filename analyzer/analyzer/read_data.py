936527bc7753e9f9b06c903360d49918d7e8b81b
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-16 18:36:00 +0200

    feat: add method to retrive data by month

--- analyzer/analyzer/read_data.py
+++ analyzer/analyzer/read_data.py
@@ -2,7 +2,7 @@ from analyzer.constants import COLUMNS
 import pandas as pd
 
 
-def read_and_clean_data():
+def read_and_clean_data() -> pd.DataFrame:
     raw_dataframe = pd.read_csv("data.csv", sep=";", index_col=False)
     raw_dataframe = raw_dataframe.rename(columns=COLUMNS)
     raw_dataframe["amount"] = raw_dataframe["raw_amount"].str.extract("([\d,-\. ]+)")
@@ -15,9 +15,26 @@ def read_and_clean_data():
     return clean_dataframe
 
 
-def get_income_data(df):
-    return df[df["amount"] > 0]
+def get_income_data(df: pd.DataFrame) -> pd.DataFrame:
+    return df[df["amount"] > 0].set_index("date")
 
 
-def get_spending_data(df):
-    return df[df["amount"] < 0]
+def get_spending_data(df: pd.DataFrame) -> pd.DataFrame:
+    return df[df["amount"] < 0].set_index("date")
+
+
+def get_income_data_per_month(df: pd.DataFrame, month_number: int) -> pd.DataFrame:
+    df = get_income_data(df)
+    return get_data_per_month(df, month_number)
+
+
+def get_spending_data_per_month(df: pd.DataFrame, month_number: int) -> pd.DataFrame:
+    df = get_income_data(df)
+    return get_data_per_month(df, month_number)
+
+
+def get_data_per_month(df: pd.DataFrame, month_number: int) -> pd.DataFrame:
+    # return df.groupby(pd.Grouper(freq="M")).sum()
+    df.loc[:, "date"] = pd.to_datetime(df["date"])
+    df[df["date"].dt.month == month_number]
+    return df
