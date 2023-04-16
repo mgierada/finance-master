ab4c432fae62f9ef08c9c3c59b85641b87545668
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-16 13:52:19 +0200

    feat: improve read and clean data

--- analyzer/analyzer/read_data.py
+++ analyzer/analyzer/read_data.py
@@ -5,8 +5,9 @@ import pandas as pd
 def read_and_clean_data():
     df = pd.read_csv("data.csv", sep=";", index_col=False)
     df = df.rename(columns=COLUMNS)
-    df["amount"] = df["raw_amount"].str.extract("([\d,\.]+)")
+    df["amount"] = df["raw_amount"].str.extract("([\d,-\. ]+)")
+    df["amount"] = df["amount"].str.replace(" ", "")
     df["amount"] = df["amount"].str.replace(",", ".").astype(float)
     df["currency"] = df["raw_amount"].str.extract("([A-Z]{3})")
-    df.drop(["raw_amount", "Unnamed: 5"], axis=1)
+    df = df.drop(["raw_amount", "Unnamed: 5"], axis=1)
     return df
