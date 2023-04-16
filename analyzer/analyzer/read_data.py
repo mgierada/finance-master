d5bc78a536b9177b9a1413b743f23b29be0200fe
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-16 13:56:48 +0200

    feat: refactor and cleanup

--- analyzer/analyzer/read_data.py
+++ analyzer/analyzer/read_data.py
@@ -3,11 +3,21 @@ import pandas as pd
 
 
 def read_and_clean_data():
-    df = pd.read_csv("data.csv", sep=";", index_col=False)
-    df = df.rename(columns=COLUMNS)
-    df["amount"] = df["raw_amount"].str.extract("([\d,-\. ]+)")
-    df["amount"] = df["amount"].str.replace(" ", "")
-    df["amount"] = df["amount"].str.replace(",", ".").astype(float)
-    df["currency"] = df["raw_amount"].str.extract("([A-Z]{3})")
-    df = df.drop(["raw_amount", "Unnamed: 5"], axis=1)
-    return df
+    raw_dataframe = pd.read_csv("data.csv", sep=";", index_col=False)
+    raw_dataframe = raw_dataframe.rename(columns=COLUMNS)
+    raw_dataframe["amount"] = raw_dataframe["raw_amount"].str.extract("([\d,-\. ]+)")
+    raw_dataframe["amount"] = raw_dataframe["amount"].str.replace(" ", "")
+    raw_dataframe["amount"] = (
+        raw_dataframe["amount"].str.replace(",", ".").astype(float)
+    )
+    raw_dataframe["currency"] = raw_dataframe["raw_amount"].str.extract("([A-Z]{3})")
+    clean_dataframe = raw_dataframe.drop(["raw_amount", "Unnamed: 5"], axis=1)
+    return clean_dataframe
+
+
+def get_income_data(df):
+    return df[df["amount"] > 0]
+
+
+def get_spending_data(df):
+    return df[df["amount"] < 0]
