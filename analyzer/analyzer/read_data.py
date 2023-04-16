6c2cecb2c136f95f173982b2d5ade491142e72b9
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-16 13:34:40 +0200

    feat: clean poetry setup

--- analyzer/analyzer/read_data.py
+++ analyzer/analyzer/read_data.py
@@ -1,12 +1,12 @@
 from analyzer.constants import COLUMNS
 import pandas as pd
 
+
 def read_and_clean_data():
-    df = pd.read_csv('data.csv', sep=';', index_col=False)
+    df = pd.read_csv("data.csv", sep=";", index_col=False)
     df = df.rename(columns=COLUMNS)
-    df['amount'] = df['raw_amount'].str.extract('([\d,\.]+)')
-    df['amount'] = df['amount'].str.replace(',', '.').astype(float)
-    df['currency'] = df['raw_amount'].str.extract('([A-Z]{3})')
+    df["amount"] = df["raw_amount"].str.extract("([\d,\.]+)")
+    df["amount"] = df["amount"].str.replace(",", ".").astype(float)
+    df["currency"] = df["raw_amount"].str.extract("([A-Z]{3})")
     df.drop(["raw_amount", "Unnamed: 5"], axis=1)
     return df
-
