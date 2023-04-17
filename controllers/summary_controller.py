f92c70d35e3934cc51e2b0172206b7a26e9581a7
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-17 16:46:35 +0200

    feat: add controller for monthly and yearly stats

--- controllers/summary_controller.py
+++ controllers/summary_controller.py
@@ -1,16 +1,32 @@
 import json
 
 from fastapi.responses import JSONResponse
-from finance_analyzer.read_data import get_summary_by_month, read_and_clean_data
+from finance_analyzer.read_data import (
+    get_summary_by_month,
+    get_summary_by_year,
+    read_and_clean_data,
+)
 
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.get("/")
-async def root():
+@router.get("/monthly")
+async def root(with_schema: bool = False) -> JSONResponse:
     df = read_and_clean_data()
     summary_by_month = get_summary_by_month(df)
     json_data = summary_by_month.to_json(orient="table")
-    return JSONResponse(content=json.loads(json_data))
+    if with_schema:
+        return JSONResponse(content=json.loads(json_data))
+    return JSONResponse(content=json.loads(json_data)["data"])
+
+
+@router.get("/yearly")
+async def root(with_schema: bool = False) -> JSONResponse:
+    df = read_and_clean_data()
+    summary_by_year = get_summary_by_year(df)
+    json_data = summary_by_year.to_json(orient="table")
+    if with_schema:
+        return JSONResponse(content=json.loads(json_data))
+    return JSONResponse(content=json.loads(json_data)["data"])
