3c23c615273df472da082387ebc68adff2c1eab4
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-20 16:45:38 +0200

    feat: retrive monthly summary using data from DB

--- src/controllers/summary_controller.py
+++ src/controllers/summary_controller.py
@@ -1,4 +1,7 @@
 import json
+import crud
+from database import get_db
+import pandas as pd
 
 from fastapi.responses import JSONResponse
 from finance_analyzer.read_data import (
@@ -7,15 +10,32 @@ from finance_analyzer.read_data import (
     read_and_clean_data,
 )
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
 @router.get("/monthly")
-async def summary_monthly(with_schema: bool = False) -> JSONResponse:
-    df = read_and_clean_data()
-    summary_by_month = get_summary_by_month(df)
+async def summary_monthly(
+    db: Session = Depends(get_db), with_schema: bool = False
+) -> JSONResponse:
+    all_transactions = crud.get_transactions(db, retrieve_all_entries=True)
+    # convert all_transactions to list of dicts
+    all_transactions = [transaction.__dict__ for transaction in all_transactions]
+    # drop _sa_instance_state and id keys
+    all_transactions = [
+        {
+            key: value
+            for key, value in transaction.items()
+            if key != "_sa_instance_state" and key != "id"
+        }
+        for transaction in all_transactions
+    ]
+
+    all_transactions_df = pd.DataFrame(all_transactions)
+    all_transactions_df["date"] = pd.to_datetime(all_transactions_df["date"])
+    summary_by_month = get_summary_by_month(all_transactions_df)
     json_data = summary_by_month.to_json(orient="table")
     if with_schema:
         return JSONResponse(content=json.loads(json_data))
