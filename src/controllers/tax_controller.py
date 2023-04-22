9ba907ff18a6373d81bf8ff74605c7042ea66d91
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-22 12:23:51 +0200

    feat: refactor to use     all_transactions_df = get_all_transactions_dataframe(db)

--- src/controllers/tax_controller.py
+++ src/controllers/tax_controller.py
@@ -3,7 +3,9 @@ import crud
 from database import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
-from services.convert_to_dataframe import convert_db_query_to_dataframe
+from services.convert_to_dataframe import (
+    get_all_transactions_dataframe,
+)
 from services.finance_analyzer.tax import get_expense_zus
 from sqlalchemy.orm import Session
 
@@ -15,10 +17,7 @@ router = APIRouter()
 async def get_zus_expenses_controller(
     db: Session = Depends(get_db), with_schema: bool = False
 ) -> JSONResponse:
-    all_transactions = crud.get_transactions(db, retrieve_all_entries=True)
-    if not all_transactions:
-        raise HTTPException(status_code=404, detail="No transactions found")
-    all_transactions_df = convert_db_query_to_dataframe(all_transactions)
+    all_transactions_df = get_all_transactions_dataframe(db)
     zus_expenses = get_expense_zus(all_transactions_df)
     json_data = zus_expenses.to_json(orient="table")
     if with_schema:
