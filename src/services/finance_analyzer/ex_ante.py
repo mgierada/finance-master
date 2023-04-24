3d0798253bf1df368698863b4ec3ee226d135850
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-24 16:44:26 +0200

    feat: refactor

--- src/services/finance_analyzer/ex_ante.py
+++ src/services/finance_analyzer/ex_ante.py
@@ -8,6 +8,7 @@ def get_ex_ante(overhours: int = 0) -> dict:
     """
     return {
         "income": get_ex_ante_income(overhours),
+        "expenses": get_ex_ante_expenses(),
     }
 
 
@@ -16,9 +17,41 @@ def get_ex_ante_income(overhours: int = 0) -> t.Dict[str, float]:
     Calculate ex ante income for the end of current month.
     """
     overhours_income = overhours * (BASED_SALARY / MONTHLY_HOUR_MEAN)
+    based_income = get_based_income()
+    overhours_income = get_overhours_income(overhours)
+    total_income = get_total_income(based_income, overhours_income)
 
     return {
-        "based_income": BASED_SALARY,
-        "overhours_income": overhours * (BASED_SALARY / MONTHLY_HOUR_MEAN),
-        "total_income": overhours_income,
+        "based_income": based_income,
+        "overhours_income": overhours_income,
+        "total_income": total_income,
+    }
+
+
+def get_based_income() -> int:
+    """
+    Calculate based income for the end of current month.
+    """
+    return BASED_SALARY
+
+
+def get_overhours_income(overhours: int = 0) -> float:
+    """
+    Calculate overhours income for the end of current month.
+    """
+    return overhours * (BASED_SALARY / MONTHLY_HOUR_MEAN)
+
+
+def get_total_income(based_income: int, overhours_income: float) -> float:
+    return based_income + overhours_income
+
+
+def get_ex_ante_expenses() -> t.Dict[str, float]:
+    """
+    Calculate ex ante expenses for the end of current month.
+    """
+    return {
+        "const_expenses": 0,
+        "var_expenses": 0,
+        "total_expenses": 0,
     }
