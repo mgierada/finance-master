874f07779a78674c24be124504e9ebe0d30d9035
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-24 11:00:30 +0200

    feat: add total endpoint for all taxes and const expenses

--- src/controllers/tax_controller.py
+++ src/controllers/tax_controller.py
@@ -9,6 +9,9 @@ from services.finance_analyzer.tax import (
     get_expenses_zus,
     get_expenses_vat7,
     get_expenses_vat_ppe,
+    get_expenses_zus_total_per_year,
+    get_expenses_vat7_total_per_year,
+    get_expenses_vat_ppe_total_per_year,
 )
 from sqlalchemy.orm import Session
 
@@ -28,13 +31,37 @@ async def get_expenses_zus_controller(
     return JSONResponse(content=json.loads(json_data)["data"])
 
 
+@router.get("/zus/total")
+async def get_expenses_zus_total_controller(
+    db: Session = Depends(get_db), with_schema: bool = False
+) -> JSONResponse:
+    all_transactions_df = get_all_transactions_dataframe(db)
+    zus_expenses_total = get_expenses_zus_total_per_year(all_transactions_df)
+    json_data = zus_expenses_total.to_json(orient="table")
+    if with_schema:
+        return JSONResponse(content=json.loads(json_data))
+    return JSONResponse(content=json.loads(json_data)["data"])
+
+
 @router.get("/vat-7")
 async def get_expenses_vat7_controller(
     db: Session = Depends(get_db), with_schema: bool = False
 ) -> JSONResponse:
     all_transactions_df = get_all_transactions_dataframe(db)
-    vat7expenses = get_expenses_vat7(all_transactions_df)
-    json_data = vat7expenses.to_json(orient="table")
+    vat7_expenses = get_expenses_vat7(all_transactions_df)
+    json_data = vat7_expenses.to_json(orient="table")
+    if with_schema:
+        return JSONResponse(content=json.loads(json_data))
+    return JSONResponse(content=json.loads(json_data)["data"])
+
+
+@router.get("/vat-7/total")
+async def get_expenses_vat7_total_controller(
+    db: Session = Depends(get_db), with_schema: bool = False
+) -> JSONResponse:
+    all_transactions_df = get_all_transactions_dataframe(db)
+    vat7_expenses_total = get_expenses_vat7_total_per_year(all_transactions_df)
+    json_data = vat7_expenses_total.to_json(orient="table")
     if with_schema:
         return JSONResponse(content=json.loads(json_data))
     return JSONResponse(content=json.loads(json_data)["data"])
@@ -47,6 +74,18 @@ async def get_expenses_vat_ppe_controller(
     all_transactions_df = get_all_transactions_dataframe(db)
     vat_ppe_expenses = get_expenses_vat_ppe(all_transactions_df)
     json_data = vat_ppe_expenses.to_json(orient="table")
+    if with_schema:
+        return JSONResponse(content=json.loads(json_data))
+    return JSONResponse(content=json.loads(json_data)["data"])
+
+
+@router.get("/vat-ppe/total")
+async def get_expenses_vat_ppe_total_controller(
+    db: Session = Depends(get_db), with_schema: bool = False
+) -> JSONResponse:
+    all_transactions_df = get_all_transactions_dataframe(db)
+    vat_ppe_expenses_total = get_expenses_vat_ppe_total_per_year(all_transactions_df)
+    json_data = vat_ppe_expenses_total.to_json(orient="table")
     if with_schema:
         return JSONResponse(content=json.loads(json_data))
     return JSONResponse(content=json.loads(json_data)["data"])
--- src/services/finance_analyzer/tax.py
+++ src/services/finance_analyzer/tax.py
@@ -1,27 +1,54 @@
 import pandas as pd
 from services.finance_analyzer.constants import ZUS, VAT_7, VAT_PPE
-from services.finance_analyzer.process_data import get_expense_data
+from services.finance_analyzer.process_data import get_expense_data, get_yearly_totals
 
 
 def get_expenses_zus(db: pd.DataFrame):
     """
     For a given cleaned dataframe, return a dataframe with the total amount of ZUS.
     """
-    expense = get_expense_data(db)
-    return expense[expense["description"].str.startswith(ZUS)]
+    expenses = get_expense_data(db)
+    return expenses[expenses["description"].str.startswith(ZUS)]
+
+
+def get_expenses_zus_total_per_year(db: pd.DataFrame):
+    """
+    For a given cleaned dataframe, return a total amount of ZUS payed yearly.
+    """
+    expenses = get_expense_data(db)
+    all_zus_expense = expenses[expenses["description"].str.startswith(ZUS)]
+    return get_yearly_totals(all_zus_expense).rename_axis("year")
 
 
 def get_expenses_vat7(db: pd.DataFrame):
     """
     For a given cleaned dataframe, return a dataframe with the total amount of VAT-7 payments.
     """
-    expense = get_expense_data(db)
-    return expense[expense["description"].str.contains(VAT_7)]
+    expenses = get_expense_data(db)
+    return expenses[expenses["description"].str.contains(VAT_7)]
+
+
+def get_expenses_vat7_total_per_year(db: pd.DataFrame):
+    """
+    For a given cleaned dataframe, return a dataframe with the total amount of VAT-7 payed yearly.
+    """
+    expenses = get_expense_data(db)
+    all_vat7_expenses = expenses[expenses["description"].str.contains(VAT_7)]
+    return get_yearly_totals(all_vat7_expenses).rename_axis("year")
 
 
 def get_expenses_vat_ppe(db: pd.DataFrame):
     """
     For a given cleaned dataframe, return a dataframe with the total amount of VAT PPE payments.
     """
-    expense = get_expense_data(db)
-    return expense[expense["description"].str.contains(VAT_PPE)]
+    expenses = get_expense_data(db)
+    return expenses[expenses["description"].str.contains(VAT_PPE)]
+
+
+def get_expenses_vat_ppe_total_per_year(db: pd.DataFrame):
+    """
+    For a given cleaned dataframe, return a dataframe with the total amount of VAT PPE payed yearly.
+    """
+    expenses = get_expense_data(db)
+    all_vat_ppe_expenses = expenses[expenses["description"].str.contains(VAT_PPE)]
+    return get_yearly_totals(all_vat_ppe_expenses).rename_axis("year")
