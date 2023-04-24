d507af62c2b18a82889a6a80e07bf70923e19c29
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-24 16:58:35 +0200

    feat: get zus expenses

--- src/services/finance_analyzer/ex_ante.py
+++ src/services/finance_analyzer/ex_ante.py
@@ -1,4 +1,8 @@
-from services.finance_analyzer.constants import BASED_SALARY, MONTHLY_HOUR_MEAN
+from services.finance_analyzer.constants import (
+    BASED_SALARY,
+    MONTHLY_HOUR_MEAN,
+    ZUS_MONTHLY_EXPENSES,
+)
 import typing as t
 
 
@@ -50,8 +54,33 @@ def get_ex_ante_expenses() -> t.Dict[str, float]:
     """
     Calculate ex ante expenses for the end of current month.
     """
+    zus_expenses = get_ex_ante_zus_expenses()
+    vat7_expenses = get_ex_ante_vat7_expenses()
+    vat_ppe_expenses = get_ex_ante_vat_ppe_expenses()
+
     return {
-        "const_expenses": 0,
-        "var_expenses": 0,
-        "total_expenses": 0,
+        "zus_expenses": zus_expenses,
+        "vat-7_expenses": vat7_expenses,
+        "vat-ppe_expenses": vat_ppe_expenses,
     }
+
+
+def get_ex_ante_zus_expenses() -> float:
+    """
+    Calculate ex ante ZUS expenses for the end of current month.
+    """
+    return ZUS_MONTHLY_EXPENSES
+
+
+def get_ex_ante_vat7_expenses() -> float:
+    """
+    Calculate ex ante VAT-7 expenses for the end of current month.
+    """
+    return 0
+
+
+def get_ex_ante_vat_ppe_expenses() -> float:
+    """
+    Calculate ex ante VAT-PPE expenses for the end of current month.
+    """
+    return 0
