04cdecf14ee8ba0e0bc0189b12fa5430e45c3697
Maciej Gierada <maciek.gierada@gmail.com>
2023-05-02 17:05:23 +0200

    feat: add expenses by categories

--- src/controllers/category_controller.py
+++ src/controllers/category_controller.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 from database import get_db
 from fastapi.responses import JSONResponse
@@ -5,18 +6,38 @@ from fastapi.responses import JSONResponse
 from fastapi import APIRouter, Depends
 from services.convert_to_dataframe import get_all_transactions_dataframe
 from services.finance_analyzer.categories import get_monthly_expenses_by_categories
-from services.finance_analyzer.process_data import get_expense_data
+from services.finance_analyzer.process_data import (
+    get_expense_data_per_month,
+)
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
+def _get_current_month_number():
+    return datetime.now().month
+
+
+def _get_current_year_number():
+    return datetime.now().year
+
+
 @router.get("/")
 async def monthly_expenses_by_categories(
     db: Session = Depends(get_db),
+    month: int = _get_current_month_number(),
+    year: int = _get_current_year_number(),
 ) -> JSONResponse:
     all_transactions_df = get_all_transactions_dataframe(db)
-    expense_data = get_expense_data(all_transactions_df)
-    monthly_expenses_by_categories = get_monthly_expenses_by_categories(expense_data)
+    monthly_expense_data = get_expense_data_per_month(all_transactions_df, month, year)
+    monthly_expenses_by_categories = get_monthly_expenses_by_categories(
+        monthly_expense_data
+    )
     json_data = monthly_expenses_by_categories.to_json(orient="table")
-    return JSONResponse(content=json.loads(json_data)["data"])
+    data = json.loads(json_data)["data"]
+    month_name = datetime.strptime(f"{month}", "%m").strftime("%B")
+    content = {
+        "message": f"Expenses by categories for month {month_name} of year {year}",
+        "data": data,
+    }
+    return JSONResponse(content=content)
--- src/services/finance_analyzer/categories.py
+++ src/services/finance_analyzer/categories.py
@@ -2,5 +2,4 @@ import pandas as pd
 
 
 def get_monthly_expenses_by_categories(df: pd.DataFrame) -> pd.DataFrame:
-    df["month"] = df["date"].dt.strftime("%Y-%m")
-    return df.groupby(["category", "month"])["amount"].sum().to_frame()
+    return df.groupby(["category"])["amount"].sum().to_frame()
