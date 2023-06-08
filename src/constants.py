05db02394238844ba715ac3513b87858912008cd
Maciej Gierada <maciek.gierada@gmail.com>
2023-06-08 10:07:42 +0200

    hotfix

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.environ.get(
-    "SQLALCHEMY_DATABASE_URL ", "fake_sqlalchemy_database_url"
+SQLALCHEMY_DATABASE_URL = str(
+    os.environ.get("SQLALCHEMY_DATABASE_URL", "fake_sqlalchemy_database_url")
 )
